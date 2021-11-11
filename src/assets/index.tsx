446694a75619fd0b312a1e19f4797ecd8bdfb2ad
Youngster Jaidev <jaidevv999@gmail.com>
2021-11-11 17:20:18 +0530

    Add the assets files

---
+++ src/assets/index.tsx
@@ -0,0 +1,9 @@
+import BearSVG from "./illustrations/BearSVG.svg";
+import Dog from "./images/Dog.jpg";
+import Bird from "./images/Bird.png";
+
+export const Illustrations = {
+  BearSVG,
+  Dog,
+  Bird
+};
